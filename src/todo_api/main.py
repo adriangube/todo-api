6e6d9a1b2f65c75d5a31f00d25debe16587d7941
adriangube <adriangubedev@gmail.com>
2024-12-16 16:59:28 +0000

    chore(config): add ruff linter + config

--- src/todo_api/main.py
+++ src/todo_api/main.py
@@ -1,17 +1,16 @@
-from typing import Union
-from fastapi import FastAPI
-import uvicorn
 import sys
+import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-  return { "Hello": "World" }
+    return {"Hello": "World"}
 
 
 def start():
-  """Launched with `poetry run start` at root level"""
-  mode = sys.argv[0] if sys.argv[0] == "prod" else "dev"
-  reload = mode == "dev"
-  uvicorn.run('src.todo_api.main:app', host='0.0.0.0', port=8000, reload=reload)
+    mode = sys.argv[0] if sys.argv[0] == "prod" else "dev"
+    reload = mode == "dev"
+    uvicorn.run("src.todo_api.main:app", host="0.0.0.0", port=8000, reload=reload)
