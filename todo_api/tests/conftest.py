f78d804543e92f76314ae43fe707b38328127b69
adriangube <adriangubedev@gmail.com>
2025-01-05 20:53:23 +0000

    add test to todo endpoints

--- todo_api/tests/conftest.py
+++ todo_api/tests/conftest.py
@@ -1,8 +1,18 @@
 import pytest
 
-from ..models import Users
+from ..models import Users, Todos
 from .db_config import TestDatabaseSession, engine
 from ..routers.auth import pwd_context
+
+@pytest.fixture(scope="function", autouse=True)
+def clean_db():
+    connection = engine.connect()
+    transaction = connection.begin()
+    TestDatabaseSession.bind = connection
+
+    yield
+    transaction.rollback()
+    connection.close()
 
 @pytest.fixture
 def test_user():
@@ -20,12 +30,16 @@ def test_user():
   yield user
 
 
-@pytest.fixture(scope="function", autouse=True)
-def clean_db():
-    connection = engine.connect()
-    transaction = connection.begin()
-    TestDatabaseSession.bind = connection
-
-    yield
-    transaction.rollback()
-    connection.close()
+@pytest.fixture
+def test_todo():
+  todo = Todos(
+      id=1,
+      title ="Test title",
+      description = "Test description",
+      complete = False,
+      user_id=1
+  )
+  db = TestDatabaseSession
+  db.add(todo)
+  db.commit()
+  yield todo
